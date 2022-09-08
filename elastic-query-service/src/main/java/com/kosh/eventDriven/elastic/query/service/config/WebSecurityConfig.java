9fb1defa28e270df4101aec3d967a96f408d25a8
koshishrijal <koshishrijal468@gmail.com>
2022-09-08 09:05:25 +0545

    basic auth added with inmemory with no encoder

--- elastic-query-service/src/main/java/com/kosh/eventDriven/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/kosh/eventDriven/elastic/query/service/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.kosh.eventDriven.elastic.query.service.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +12,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll();
+
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .hasRole("USER")
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("USER");
     }
 }
